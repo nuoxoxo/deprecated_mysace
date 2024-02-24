ee9f73208c4242710f454e38c993d0c3e33aa963
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-02-24 23:22:33 +0100

    init/does it work

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+//export default defineConfig({});
+
+export default defineConfig({
+  site: 'https://nuoxoxo.github.io',
+  base: '/bandcamper', // don't set if no path after `io/`
+})
